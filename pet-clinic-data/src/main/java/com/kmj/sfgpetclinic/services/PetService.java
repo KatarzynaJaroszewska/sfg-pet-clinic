17d06d9c354182aa2e293ab274bbe0002ea0e708
Katarzyna Jaroszewska <katarzyna_jaroszewska@wp.eu>
2020-09-07 22:48:12 +0200

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/kmj/sfgpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package com.kmj.sfgpetclinic.services;
+
+import com.kmj.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/kmj/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package com.kmj.sfgpetclinic.services;
+
+import com.kmj.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
